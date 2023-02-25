6efe20ba24fbe71bb0994d73454cf41874889197
apezord <apezord@proton.me>
2023-02-25 11:29:03 -0700

    cleaner errors

--- doginals.js
+++ doginals.js
@@ -514,4 +514,7 @@ function server() {
 }
 
 
-main().catch(e => console.error(e.toString(), e.response && e.response.data && e.response.data.error))
+main().catch(e => {
+    let reason = e.response && e.response.data && e.response.data.error && e.response.data.error.message
+    console.error(reason ? e.message + ':' + reason : e.message)
+})
